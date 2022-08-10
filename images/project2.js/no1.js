7db2d1cd450819427dfaa0b9597aa737e7c6300d
Felix <cmekusfelix8@gmail.com>
2022-08-10 07:20:57 +0100

    initial

---
+++ images/project2.js/no1.js
@@ -0,0 +1,9 @@
+//Addition
+var a = 3; //no. 3 is being assigned to variable a(right-to-left associativity)
+var b = 3;
+var c = 6;
+
+var result = 'Felix' + a + b  ;  //left to right
+
+document.write('<h1>Result is: '+result+ '</h1>')
+
